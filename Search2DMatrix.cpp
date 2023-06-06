5d3c9404baf8fc24b02e643a8992524a349f03a8
Jigyashu <jigyashusingh01@gmail.com>
2023-06-06 13:28:55 +0530

    Create Search2DMatrix.cpp

---
+++ Search2DMatrix.cpp
@@ -0,0 +1,16 @@
+//Striver Sheet Challenge 
+
+bool searchMatrix(vector<vector<int>>& mat, int target) {
+        int row = mat.size();
+        int col = mat[0].size();
+        int i=0 ,j=col-1;
+        while(i<row && j>=0){
+            if(mat[i][j] == target)
+                return true;
+            else if(mat[i][j] > target)
+                j--;
+            else if(mat[i][j] < target)
+                i++;
+        }
+        return false;
+}
